915da8a3fe87cd7c9065c2d697949c79072ac0c1
Brandon Alan Rodríguez <br4nd0n4l4n@gmail.com>
2016-09-26 16:40:27 -0500

    Nuevas redirecciones

--- js/main.js
+++ js/main.js
@@ -1,90 +1,15 @@
-(function($){
-    $.fn.scrollingTo = function( opts ) {
-        var defaults = {
-            animationTime : 1000,
-            easing : '',
-            callbackBeforeTransition : function(){},
-            callbackAfterTransition : function(){}
-        };
-
-        var config = $.extend( {}, defaults, opts );
-
-        $(this).click(function(e){
-            var eventVal = e;
-            e.preventDefault();
-
-            var $section = $(document).find( $(this).data('section') );
-            if ( $section.length < 1 ) {
-                return false;
-            };
-
-            if ( $('html, body').is(':animated') ) {
-                $('html, body').stop( true, true );
-            };
-
-            var scrollPos = $section.offset().top;
-
-            if ( $(window).scrollTop() == scrollPos ) {
-                return false;
-            };
-
-            config.callbackBeforeTransition(eventVal, $section);
-
-            $('html, body').animate({
-                'scrollTop' : (scrollPos+'px' )
-            }, config.animationTime, config.easing, function(){
-                config.callbackAfterTransition(eventVal, $section);
-            });
-        });
-    };
-}(jQuery));
-
-
-
-jQuery(document).ready(function(){
-	"use strict";
-	new WOW().init();
-
-
-(function(){
- jQuery('.smooth-scroll').scrollingTo();
-}());
-
-});
-
-
-
-
 $(document).ready(function(){
-
-
-
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
-            $("#top-bar").removeClass("animated-header");
-        } else {
-            $(".navbar-brand a").css("color","inherit");
-            $("#top-bar").addClass("animated-header");
-        }
-    });
-
-    $("#clients-logo").owlCarousel({
- 
-        itemsCustom : false,
-        pagination : false,
-        items : 5,
-        autoplay: true,
-
-    })
-
-});
-
-
-
-// fancybox
-$(".fancybox").fancybox({
+  "use strict";
+  new WOW().init();
+  $('.smooth-scroll').scrollingTo();
+  $("#clients-logo").owlCarousel({
+    itemsCustom : false,
+    pagination : false,
+    items : 5,
+    autoplay: true
+  });
+  // fancybox
+  $(".fancybox").fancybox({
     padding: 0,
 
     openEffect : 'elastic',
@@ -95,24 +20,65 @@ $(".fancybox").fancybox({
 
     closeClick : true,
     helpers : {
-        title : { 
-            type: 'inside' 
-        },
-        overlay : {
-            css : {
-                'background' : 'rgba(0,0,0,0.8)'
-            }
+      title : {
+        type: 'inside'
+      },
+      overlay : {
+        css : {
+          'background' : 'rgba(0,0,0,0.8)'
         }
+      }
     }
+  });
+});
+$(document).off('click','.place_link').on('click','.place_link',function() {
+  $('#contenido_change').empty().load($(this).find('a').data('href'));
+});
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 50) {
+    $(".navbar-brand a").css("color","#fff");
+    $("#top-bar").removeClass("animated-header");
+  } else {
+    $(".navbar-brand a").css("color","inherit");
+    $("#top-bar").addClass("animated-header");
+  }
 });
 
+$.fn.scrollingTo = function( opts ) {
+  var defaults = {
+    animationTime : 1000,
+    easing : '',
+    callbackBeforeTransition : function(){},
+    callbackAfterTransition : function(){}
+  };
 
+  var config = $.extend( {}, defaults, opts );
 
+  $(this).click(function(e){
+    var eventVal = e;
+    e.preventDefault();
 
+    var $section = $(document).find( $(this).data('section') );
+    if ( $section.length < 1 ) {
+      return false;
+    };
 
+    if ( $('html, body').is(':animated') ) {
+      $('html, body').stop( true, true );
+    };
 
- 
+    var scrollPos = $section.offset().top;
 
+    if ( $(window).scrollTop() == scrollPos ) {
+      return false;
+    };
 
+    config.callbackBeforeTransition(eventVal, $section);
 
-
+    $('html, body').animate({
+      'scrollTop' : (scrollPos+'px' )
+    }, config.animationTime, config.easing, function(){
+      config.callbackAfterTransition(eventVal, $section);
+    });
+  });
+};

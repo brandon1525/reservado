82c4cdebc6afda87f33dbb824cb2313902b55206
Brandon Alan Rodríguez <br4nd0n4l4n@gmail.com>
2016-10-04 16:20:28 -0500

    Conflictos

---
+++ app/js/customer_index.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  user_exist();
+  set_title_nav_side();
+  set_bar_elements();
+});
+function set_bar_elements(){
+  var user_data = JSON.parse(localStorage.getItem('user_data'));
+  $.when(charge_places()).done(function(json){
+    if(json.success){
+      $.each(json.places, function(index, value){
+        var data_append = '<li class="no-padding">'+
+        '<ul class="collapsible collapsible-accordion">'+
+        '<li>'+
+        '<a class="collapsible-header"> '+index+' <i class="material-icons">arrow_drop_down</i></a>'+
+        '<div class="collapsible-body">'+
+        '<ul>';
+        $.each(value, function(index2, value2){
+          data_append+='<li><a href="#!">'+value2.name+'</a></li>';
+        });
+        data_append+='</ul>'+
+        '</div>'+
+        '</li>'+
+        '</ul>'+
+        '</li>';
+        $('#charge_places').append(data_append);
+      });
+    }else{
+      Materialize.toast(json.msg, 3000, 'rounded red');
+    }
+    $('#charge_places .collapsible').collapsible({
+      accordion : false
+    });
+  });
+}
+
+function charge_places(){
+  return $.ajax({
+    url: "http://localhost:3000/places/grouped",
+    method: "GET"
+  });
+}
